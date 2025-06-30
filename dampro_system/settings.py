ed8d9e1b35369ae9200988814bf48be9d008b8c7
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2025-06-30 04:50:34 -0500

    añadiendo permisos url

--- dampro_system/settings.py
+++ dampro_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-d2&96j)tfe_dbif-g6@&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
